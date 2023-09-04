ac44b254ee96394ef7bcc17b051b92d0b2845407
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-04 15:31:46 +0100

    move links above scripts

--- jsx/mod.ts
+++ jsx/mod.ts
@@ -13,8 +13,8 @@ export const Content = (
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>${title}</title>
     <link rel="preload" href="/static/htmx.org@1.9.5.min.js" as="script" />
-    <script src="/static/htmx.org@1.9.5.min.js" defer></script>
     <link rel="preload" href="/static/index.js" as="script" />
+    <script src="/static/htmx.org@1.9.5.min.js" defer></script>
     <script type="module" src="/static/index.js" defer></script>
 </head>
 <body hx-boost="${true}">
