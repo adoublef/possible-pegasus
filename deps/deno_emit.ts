fc9fa9d039206c27f9773d19b0c955f25bb682cf
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-04 11:28:51 +0100

    update deps

---
+++ deps/deno_emit.ts
@@ -0,0 +1,1 @@
+export { bundle, transpile } from "https://deno.land/x/emit@0.27.0/mod.ts";
---
+++ deps/hono.ts
@@ -0,0 +1,15 @@
+export {
+    Hono,
+    HTTPException,
+    type ErrorHandler,
+    type MiddlewareHandler,
+} from "https://deno.land/x/hono@v3.5.7/mod.ts";
+export { type H as Handler } from "https://deno.land/x/hono@v3.5.7/types.ts";
+export {
+    html,
+    logger,
+    memo,
+    raw ,
+    serveStatic,
+} from "https://deno.land/x/hono@v3.5.7/middleware.ts";
+export { type HtmlEscapedString as HtmlEscapedString } from "https://deno.land/x/hono@v3.5.7/utils/html.ts";
