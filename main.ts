b8d3f99f8647f60c557415ae1e08a82301bd75d7
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-04 11:47:00 +0100

    import script via endpoint

--- main.ts
+++ main.ts
@@ -1,24 +1,25 @@
-import { Hono, html, raw } from "$deps/hono.ts";
+import { Hono, html } from "$deps/hono.ts";
 import { bundle } from "$deps/deno_emit.ts";
-
-const { code } = await bundle(
-    new URL("./components/mod.ts", import.meta.url),
-    { cacheRoot: Deno.cwd() }
-);
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
     const app = new Hono();
 
+    app.get("/index.js", async (c) => {
+        const { code } = await bundle(
+            new URL("./components/mod.ts", import.meta.url),
+            { cacheRoot: Deno.cwd() },
+        );
+        return c.text(code);
+    });
+
     app.get("/", (c) => {
         return c.html(html`
+            <script src="/index.js" defer></script>
             <body>
                 <hello-world></hello-world>
                 <hello-world name="Rahim"></hello-world>
             </body>
-            <script>
-                ${raw(code)}
-            </script>
         `);
     });
 
