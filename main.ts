6f8ce7b3dde23d4a44a21c0b96dc2e75f7a6ec1f
Kristopher Rahim Afful-Brown <kristopherab@gmail.com>
2023-09-04 11:29:27 +0100

    main script uses wc

---
+++ main.ts
@@ -0,0 +1,25 @@
+import { Hono, html, raw } from "$deps/hono.ts";
+import { bundle } from "$deps/deno_emit.ts";
+
+const { code } = await bundle(
+    new URL("./components/mod.ts", import.meta.url),
+);
+
+// Learn more at https://deno.land/manual/examples/module_metadata#concepts
+if (import.meta.main) {
+    const app = new Hono();
+
+    app.get("/", (c) => {
+        return c.html(html`
+            <body>
+                <hello-world></hello-world>
+                <hello-world name="Rahim"></hello-world>
+            </body>
+            <script>
+                ${raw(code)}
+            </script>
+        `);
+    });
+
+    Deno.serve(app.fetch);
+}
